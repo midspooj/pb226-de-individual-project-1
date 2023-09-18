fc3e49a685c116da3ea51aecdabd686920641dad
midspooj <142264378+midspooj@users.noreply.github.com>
2023-09-17 23:37:04 -0400

    Create descriptive_stats.py
    this is the python script to call all the functions inside lib.py

---
+++ pythonproject/src/descriptive_stats.py
@@ -0,0 +1,17 @@
+import lib
+
+def execute_all_functions():
+    # Load CSV Data
+    og_csv_path = r'https://raw.githubusercontent.com/midspooj/pb226-de-miniproject-3/main/country_wise_latest.csv'
+    df = lib.load_csv_data(og_csv_path)
+
+    # Visualize Scatter Plot
+    lib.visualize_scatter_plot(df)
+
+    # Calculate Summary Statistics
+    summary_stats = lib.calculate_summary_statistics(df)
+    return summary_stats
+
+if __name__ == "__main__":
+    results = execute_all_functions()
+    print(results)

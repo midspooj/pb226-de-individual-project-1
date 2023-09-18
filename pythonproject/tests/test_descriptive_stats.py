bf9701f7b9de93a9180d9381ec8fc9280c4bfc74
midspooj <142264378+midspooj@users.noreply.github.com>
2023-09-18 17:53:28 -0400

    Create test_descriptive_stats.py

---
+++ pythonproject/tests/test_descriptive_stats.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+
+# Add the relative path to the "src" folder so that we can import descriptive_stats.py
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+# Import the functions from descriptive_stats.py
+from descriptive_stats import execute_all_functions
+
+def test_execute_all_functions():
+    # Execute all functions
+    results = execute_all_functions()
+
+    #Add your assert statements here
+    # For example:
+    assert isinstance(results, <polars.dataframe.frame.DataFrame>), "Error: Incorrect data type returned"
+
+if __name__ == '__main__':
+    test_execute_all_functions()
